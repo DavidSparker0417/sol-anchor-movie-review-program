bc6e6b2fc593207bd35c53594bf412e3674267b0
ilesoviy <ilisovij861@gmail.com>
2024-05-07 17:06:26 +0300

    change the project title

--- programs/anchor-movie-review-program/src/lib.rs
+++ programs/anchor-movie-review-program/src/lib.rs
@@ -1,9 +1,9 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Ht7WfCZuS4PtvboEzNRhSYs7n7Jw2FGKPMwjHu6bmcbr");
+declare_id!("HfWWkTo2woxAZYVaTgwxG9ZcGTnouQegbtnsZv1vFN4r");
 
 #[program]
-pub mod anchor_review_program {
+pub mod anchor_movie_review_program {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
